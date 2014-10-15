621de8109f1a132b1cd0557aaafd56226c0dc78b
hyloguy <michael@polyblog.com>
2014-10-15 15:21:52 -0700

    Implemented cart - adding, emptying, and total with pluralize

--- app/books_controller.js
+++ app/books_controller.js
@@ -5,7 +5,23 @@ bookly.bookApp = angular.module("bookApp", []);
 bookly.BooksController = function($scope) {
   // load books. They are defined in data.js (we've got no back end yet!)
   $scope.books = books;
+  $scope.cart = $scope.cart || [];
 
   //Code to manage cart goes here
+  $scope.addBookToCart = function(book) {
+  	$scope.cart.push(book);
+  };
+
+  $scope.getTotalInCart = function() {
+  	total = 0;
+  	for (i=0; i<$scope.cart.length; i++) {
+  		total += $scope.cart[i].price;
+  	}
+  	return total;
+  };
+
+  $scope.emptyCart = function() {
+  	$scope.cart = [];
+  }
 
 };
